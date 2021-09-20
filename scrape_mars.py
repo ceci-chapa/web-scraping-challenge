1a49008d64fbbcae05381afe9dfa5a179bfe40c9
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-09-20 01:23:14 -0500

    editing scrap py file

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,6 @@
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import time
+from bs4 import BeautifulSoup
+from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -9,38 +9,29 @@ def scrape_info():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    # NASA Mars News - Getting the titles --------------
+    url = 'https://redplanetscience.com/'
     browser.visit(url)
-
-    time.sleep(1)
-
-    # Scrape page into Soup
     html = browser.html
-    soup = bs(html, "html.parser")
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
+    results = soup.find_all('div', class_='list_text')
+    news_title = results[0].find('div', class_='content_title').text
+    news_p = results[0].find('div', class_='article_teaser_body').text
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
 
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
+
+    
+
+    #Make a dictionary to contain all variables to use later in my app/html
+
+
 
     # Close the browser after scraping
     browser.quit()
 
-    # Return results
-    return costa_data
+    # Return results from the dictionary variable
+    return 
